22f428446db794f39b3402fed1e5c679d3ef6fbc
S.-C. Lee <teamwork@kaist.ac.kr>
2017-07-24 18:57:03 +0900

    read me and ki_api.dart

--- lib/outer_api/ki_api.dart
+++ lib/outer_api/ki_api.dart
@@ -1,16 +1,19 @@
 import 'package:http/http.dart' as http;
 import 'dart:async';
+import 'dart:convert';
 
 class KIApi {
   static String baseURL = "http://localhost:8080";
 
   Future<Null> sendChatMessage(String sessionID, String message) async{
-    var response = await http.post(
-      baseURL + "/chat",
-      body: {
+    var payload  = {
         "room": sessionID,
         "message": message
-      }
+    };
+
+    var response = await http.post(
+      baseURL + "/chat",
+      body: JSON.encode(payload)
     );
     return response.statusCode == 200;
   }
